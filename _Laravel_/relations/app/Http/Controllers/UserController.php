71cd514d53ae67461b8c01bbe627eb6dcdd02f66
Rodolfo Freire Belo <rodolfo.belo87@gmail.com>
2024-10-10 13:28:26 -0300

    Relations Controleller Migration
    Controllers com list all e list one

--- _Laravel_/relations/app/Http/Controllers/UserController.php
+++ _Laravel_/relations/app/Http/Controllers/UserController.php
@@ -7,8 +7,60 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(Request $r){
-        $users = User::all();
-        return $users;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $r)
+    {
+        return $users = User::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return $user = User::find($id);
+        // return $user;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
--- _Laravel_/relations/routes/api.php
+++ _Laravel_/relations/routes/api.php
@@ -5,5 +5,8 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', [UserController::class, 'index']);
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+
 Route::get('/addresses', [AddressController::class, 'index']);
+Route::get('/addresses/{id}', [AddressController::class, 'show']);
